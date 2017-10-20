1434ea45bac903cdd1622e0a6d215a879b8ae36a
edmasson <edmasson@sodifrance.fr>
2017-10-20 14:09:23 +0200

    Mise en place de commentaires

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -10,16 +10,16 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/toPromise';
   
-
+//Webservice utilisant ihni, fonctionnel 
 @Injectable()
 export class SrvUserService {
 
   urlUser='http://192.168.72.105/api/user/';
 
   constructor(private _http: Http) {  }
-
+//On passe l'identifiant souhaité et l'apikey de hni (vérifier que l'APIkey est bien à jour )
   getUserById(id,apikey): Observable<User> { 
-    //return  this._http.get(this.urlFiche+'id').map((response : Response)=> <Fiche[]> response.json()).toPromise();
+ 
     return  this._http.get(this.urlUser+id+'?apikey='+apikey).map((response : Response)=> <User> response.json()).catch(this.httpError);
   }
 
--- src/app/starter/starter-list/list-main/list-main.component.ts
+++ src/app/starter/starter-list/list-main/list-main.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../../../class/user'
-import {Skill} from '../../../class/skill'
-import {SelectModule} from 'ng2-select';
-import {SrvSkillService} from '../../../shared/skill.service'
+import { User } from '../../../class/user'
+import { Skill } from '../../../class/skill'
+import { SelectModule } from 'ng2-select';
+import { SrvSkillService } from '../../../shared/skill.service'
 // Variable in assets/js/scripts.js file
 declare var AdminLTE: any;
 
@@ -15,48 +15,52 @@ declare var AdminLTE: any;
 
 
 export class ListMainComponent implements OnInit {
-  
-  constructor(private skillSrv: SrvSkillService) { 
-    
-      }
-skills: Skill[] =[];
 
-selectedSkill: Skill;
-      add(name: string): void {
-        name = name.trim();
-        if (!name) { return; }
-        this.skillSrv.create(name)
-          .then(skill => {
-            this.skills.push(skill);
-            this.selectedSkill = null;
-          });
-      }
-      getAllSkills(): void {
-        this.skillSrv
-            .getAllSkills()
-            .then(skills => this.skills = skills);
-      }
+  constructor(private skillSrv: SrvSkillService) {
+
+  }
+  skills: Skill[] = [];
+
+  selectedSkill: Skill;
+//Ajout d'une compétence utilisateur dans la liste 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.skillSrv.create(name)
+      .then(skill => {
+        this.skills.push(skill);
+        this.selectedSkill = null;
+      });
+  }
+  //Récupération de toutes les compétences 
+  getAllSkills(): void {
+    this.skillSrv
+      .getAllSkills()
+      .then(skills => this.skills = skills);
+  }
 
   role = 'pilote';
-
-    public items:Array<string> = ['C++', 'C#', 'oracle', 'grails',
+// liste déroulante utilisé dans select 2 à remplacer par celle de l'api des compétences 
+  public items: Array<string> = ['C++', 'C#', 'oracle', 'grails',
     'java', 'J2E', '.net', 'Management', 'PERT', 'GANTT',
-    ];
- 
-  private value:any = {};
-  private _disabledV:string = '0';
-  private disabled:boolean = false;
- 
-  private get disabledV():string {
+  ];
+
+  //fonctions préconfiguré de select 2 (à supprimer si non utilisés
+  private value: any = {};
+  private _disabledV: string = '0';
+  private disabled: boolean = false;
+
+  private get disabledV(): string {
     return this._disabledV;
   }
- 
-  private set disabledV(value:string) {
+
+  private set disabledV(value: string) {
     this._disabledV = value;
     this.disabled = this._disabledV === '1';
   }
- 
-  public selected(value:any):void {
+
+  //ajout d'une compétence dans la liste au clic sur select 2 
+  public selected(value: any): void {
     console.log(value.id);
     if (!value.id) { return; }
     this.skillSrv.create(value.id)
@@ -67,25 +71,25 @@ selectedSkill: Skill;
 
     console.log('Selected value is: ', value);
   }
- 
-  public removed(value:any):void {
+
+  public removed(value: any): void {
     console.log('Removed value is: ', value);
   }
- 
-  public typed(value:any):void {
+
+  public typed(value: any): void {
     console.log('New search input: ', value);
   }
- 
-  public refreshValue(value:any):void {
+
+  public refreshValue(value: any): void {
     this.value = value;
   }
 
 
-  ngOnInit() : void {
+  ngOnInit(): void {
     this.getAllSkills();
-  
+
     // Update the AdminLTE layouts
-  
+
     AdminLTE.init();
   }
 
